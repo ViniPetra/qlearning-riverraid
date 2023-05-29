814271c156b15d26de3ec90deb5b5cfe48a48a68
Vinicius Petratti <viniciuspetratti@hotmail.com>
2023-05-29 19:19:06 -0300

    Etapa 1

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import gymnasium
 
-env = gymnasium.make("ALE/Tetris-v5", render_mode="human", obs_type="ram")
+env = gymnasium.make("ALE/Riverraid-v5", render_mode="human", obs_type="ram")
 observation, info = env.reset(seed=42)
-#for _ in range(1000):
+
+def convert(n):
+  out = 0
+  for bit in n:
+    out = (out << 1) | bit
+  return out
+
 while True:
-  # definição da política
   acao = env.action_space.sample()
 
   ambiente, recompensa, finalizado, paralizado, info = env.step(acao)
+
+  ambiente_txt = convert(ambiente)
   recompensa_txt = "Recompensa: [%s]" % recompensa
   fim_txt = "Finalizado: [%s]" % finalizado
   acao_txt = "Ação: [%s]" % acao
@@ -20,9 +27,9 @@ while True:
   if (finalizado):
     fim_txt = fim_txt
 
-  print(recompensa_txt, fim_txt, acao_txt)
+  print(ambiente_txt, recompensa_txt, fim_txt, acao_txt)
 
   if finalizado or paralizado:
     break
-    #ambiente, info = env.reset()
+
 env.close()
